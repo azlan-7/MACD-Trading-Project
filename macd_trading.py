7a92cd02562e373aa7534eb189e745f8fb4035bf
Azlan Siddiqui <42199139+azlan-7@users.noreply.github.com>
2022-03-02 13:19:26 +0530

    Add files via upload

---
+++ macd_trading.py
@@ -0,0 +1,61 @@
+
+import requests
+import pandas as pd
+import numpy as np
+from math import floor
+from termcolor import colored as cl
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (20, 10)
+plt.style.use('fivethirtyeight')
+
+def get_historical_data(symbol, start_date = None):
+    api_key = 'Y2AJESBAGTJ3X5JK'
+    api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}&outputsize=full'
+    raw_df = requests.get(api_url).json()
+    df = pd.DataFrame(raw_df[f'Time Series (Daily)']).T
+    df = df.rename(columns = {'1. open': 'open', '2. high': 'high', '3. low': 'low', '4. close': 'close', '5. adjusted close': 'adj close', '6. volume': 'volume'})
+    for i in df.columns:
+        df[i] = df[i].astype(float)
+    df.index = pd.to_datetime(df.index)
+    df = df.iloc[::-1].drop(['7. dividend amount', '8. split coefficient'], axis = 1)
+    if start_date:
+        df = df[df.index >= start_date]
+    return df
+
+googl = get_historical_data('GOOGL', '2020-01-01')
+print(googl)
+
+
+def get_macd(price, slow, fast, smooth):
+    exp1 = price.ewm(span = fast, adjust = False).mean()
+    exp2 = price.ewm(span = slow, adjust = False).mean()
+    macd = pd.DataFrame(exp1 - exp2).rename(columns = {'close':'macd'})
+    signal = pd.DataFrame(macd.ewm(span = smooth, adjust = False).mean()).rename(columns = {'macd':'signal'})
+    hist = pd.DataFrame(macd['macd'] - signal['signal']).rename(columns = {0:'hist'})
+    frames =  [macd, signal, hist]
+    df = pd.concat(frames, join = 'inner', axis = 1)
+    return df
+
+googl_macd = get_macd(googl['close'], 26, 12, 9)
+print(googl_macd)
+
+
+
+def plot_macd(prices, macd, signal, hist):
+    ax1 = plt.subplot2grid((8,1), (0,0), rowspan = 5, colspan = 1)
+    ax2 = plt.subplot2grid((8,1), (5,0), rowspan = 3, colspan = 1)
+
+    ax1.plot(prices)
+    ax2.plot(macd, color = 'grey', linewidth = 1.5, label = 'MACD')
+    ax2.plot(signal, color = 'skyblue', linewidth = 1.5, label = 'SIGNAL')
+
+    for i in range(len(prices)):
+        if str(hist[i])[0] == '-':
+            ax2.bar(prices.index[i], hist[i], color = '#ef5350')
+        else:
+            ax2.bar(prices.index[i], hist[i], color = '#26a69a')
+
+    plt.legend(loc = 'lower right')
+
+print(plot_macd(googl['close'], googl_macd['macd'], googl_macd['signal'], googl_macd['hist']))
